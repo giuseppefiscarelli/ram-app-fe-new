de378e36cf73f3daba9aadabec988a910ff26269
giuseppefiscarelli <fiscarelli.giu@gmail.com>
2023-07-19 21:18:06 +0200

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,6 +16,10 @@ export class AppComponent {
     if (this.swUpdate.isEnabled) {
       this.swUpdate.versionUpdates.subscribe((event: VersionEvent) => {
        console.log(event)
+       if(confirm("New version available. Load New Version?")) {
+
+        window.location.reload();
+    }
       });
 
       // Controlla se ci sono aggiornamenti disponibili
