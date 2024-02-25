8fdf8a2927816ba27613f612961711f1f144d770
giuseppefiscarelli <fiscarelli.giu@gmail.com>
2024-02-25 18:57:29 +0100

    Update report.service.ts

--- src/app/modules/features/config/report.service.ts
+++ src/app/modules/features/config/report.service.ts
@@ -258,7 +258,7 @@ constructor(private http:HttpClient,  private API: ApiService,) { }
             { text: ' abbiamo necessità di ricevere i seguenti chiarimenti e/o documenti:'}
           ], alignment:'justify'
         },
-        {ul:dataReport['detail'], margin:[20,10], bold:true},
+        {ul:dataReport['detail'], margin:[0,10], alignment:'justify', bold:true},
           {
             text:[
               {text: 'Pertanto, ai sensi e per gli effetti dell\'art. 10, comma 4 del D.D 12 aprile 2022 n.155, Vi invitiamo a fornirci la suddetta documentazione '},
