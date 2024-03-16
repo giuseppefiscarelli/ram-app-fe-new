a43900bcc288be521a39572eeec3101c8df4e9a0
giuseppefiscarelli <fiscarelli.giu@gmail.com>
2024-03-16 13:13:43 +0100

    1603.01

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,7 +8,8 @@
 }; */
 
 export const environment = {
-  endpoint: 'https://elevatasostenibilita.ramspa.it/api/',
+  endpoint:'http://localhost:1337',
+//  endpoint: 'https://elevatasostenibilita.ramspa.it/api/',
 //  endpoint: 'http://3.122.31.243:1337/',
   baseApi: null,
   production: false
