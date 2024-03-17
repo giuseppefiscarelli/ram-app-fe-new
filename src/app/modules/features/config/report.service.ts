fd411ea39b24d5593dc866adfa7b11caf9c28619
giuseppefiscarelli <fiscarelli.giu@gmail.com>
2024-03-17 20:21:12 +0100

    Update report.service.ts

--- src/app/modules/features/config/report.service.ts
+++ src/app/modules/features/config/report.service.ts
@@ -497,7 +497,7 @@ constructor(private http:HttpClient,  private API: ApiService,) { }
               { text:'Art.3, comma 1, lett a)', alignment:'center',margin:[0,10]},
               {text:myGroupedData.find(x=> x.artDm === '1A')?.numAccepted},
               {text:new Intl.NumberFormat('it-IT', { style: 'currency', currency: 'EUR' }).format(myGroupedData.find(x=> x.artDm === '1A')?.totalAmount ||0)},
-              {text: myGroupedData.find(x=> x.artDm === '1A')?.totalFinanziamento>0 && istanza.rete && istanza.pmi && istanza.pmi === 'Yes' && istanza.rete === 'Yes'?'20%': istanza.rete === 'Yes' ||istanza.pmi === 'Yes'?'10%':null},
+              {text: myGroupedData.find(x=> x.artDm === '1A')?.totalFinanziamento>0 ?(istanza.rete && istanza.rete === 'Yes') && (istanza.pmi && istanza.pmi === 'Yes') ? '20%' : istanza.rete === 'Yes' ||istanza.pmi === 'Yes'?'10%':null:null},
               {text:new Intl.NumberFormat('it-IT', { style: 'currency', currency: 'EUR' }).format(myGroupedData.find(x=> x.artDm === '1A')?.totalFinanziamento ||0)},
 
 
@@ -507,7 +507,7 @@ constructor(private http:HttpClient,  private API: ApiService,) { }
               { text:'Art.3, comma 1, lett b)', alignment:'center',margin:[0,10]},
               {text:myGroupedData.find(x=> x.artDm === '1B')?.numAccepted},
               {text:new Intl.NumberFormat('it-IT', { style: 'currency', currency: 'EUR' }).format(myGroupedData.find(x=> x.artDm === '1A')?.totalAmount ||0)},
-              {text: myGroupedData.find(x=> x.artDm === '1B')?.totalFinanziamento>0 && istanza.rete && istanza.pmi && istanza.pmi === 'Yes' && istanza.rete === 'Yes'?'20%': istanza.rete === 'Yes' ||istanza.pmi === 'Yes'?'10%':null},
+              {text: myGroupedData.find(x=> x.artDm === '1B')?.totalFinanziamento>0?(istanza.rete && istanza.rete === 'Yes') && (istanza.pmi && istanza.pmi === 'Yes') ? '20%' : istanza.rete === 'Yes' ||istanza.pmi === 'Yes'?'10%':null:null},
               {text:new Intl.NumberFormat('it-IT', { style: 'currency', currency: 'EUR' }).format(myGroupedData.find(x=> x.artDm === '1A')?.totalFinanziamento ||0)},
 
 
@@ -515,7 +515,7 @@ constructor(private http:HttpClient,  private API: ApiService,) { }
             [
               {text:'Maggiorazione Rottamazione', colSpan:2, alignment:'center', bold:true},'',
               {text:'%', colSpan:2, alignment:'center', bold:true},'',
-              {text:'€', },
+              { text:new Intl.NumberFormat('it-IT', { style: 'currency', currency: 'EUR' }).format(maggiorazioneRottamazione)},
             ],
 
 
