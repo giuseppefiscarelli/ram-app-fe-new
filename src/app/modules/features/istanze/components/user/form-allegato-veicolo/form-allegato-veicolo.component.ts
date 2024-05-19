bf078950b9b5b40495fcef0e15aec13b6cbd9c55
giuseppefiscarelli <57521642+giuseppefiscarelli@users.noreply.github.com>
2024-05-19 12:41:45 +0200

    Update form-allegato-veicolo.component.ts

--- src/app/modules/features/istanze/components/user/form-allegato-veicolo/form-allegato-veicolo.component.ts
+++ src/app/modules/features/istanze/components/user/form-allegato-veicolo/form-allegato-veicolo.component.ts
@@ -65,7 +65,11 @@ export class FormAllegatoVeicoloComponent implements OnInit {
 
 
                       if(this.mode === 'create'){
-                          this.filteredType = this.typeDocuments.filter( (x)=> this.docList.includes(x.id))
+                        if(this.veicolo.acquisitionType === '01'){
+                          this.filteredType = this.typeDocuments.filter( (x)=> this.docList.includes(x.id) && x.id !== 9);
+                        }else{
+                          this.filteredType = this.typeDocuments.filter( (x)=> this.docList.includes(x.id));
+                        }
                           this.listOfPlaceholder=[];
                           this.dialogTitle = 'Inserimento Allegato Veicolo';
                           this.btnSubmit= 'Salva Allegato';
@@ -73,7 +77,11 @@ export class FormAllegatoVeicoloComponent implements OnInit {
                       }
                       if(this.mode === 'edit'){
                           this.listOfPlaceholder=[];
-                          this.filteredType = this.typeDocuments.filter( (x)=> this.docList.includes(x.id))
+                          if(this.veicolo.acquisitionType === '01'){
+                            this.filteredType = this.typeDocuments.filter( (x)=> this.docList.includes(x.id) && x.id !== 9);
+                          }else{
+                            this.filteredType = this.typeDocuments.filter( (x)=> this.docList.includes(x.id));
+                          }
                           this.dialogTitle = 'Aggiornamento Allegato Veicolo';
                           this.btnSubmit= 'Aggiorna Allegato';
                           this.form = new FormGroup({});
