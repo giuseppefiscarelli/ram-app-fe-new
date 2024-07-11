4759b44b854b7980cc882325bc8d6f4746f0d89f
giuseppefiscarelli <57521642+giuseppefiscarelli@users.noreply.github.com>
2024-07-11 15:28:56 +0200

    Update admin-veicolo-dialog.component.ts

--- src/app/modules/features/istanze/components/admin/admin-veicolo-dialog/admin-veicolo-dialog.component.ts
+++ src/app/modules/features/istanze/components/admin/admin-veicolo-dialog/admin-veicolo-dialog.component.ts
@@ -427,7 +427,18 @@ export class AdminVeicoloDialogComponent implements OnInit {
             //     );
 
 
+            this.services.getFile(file)
+            .subscribe(
+            (res) => {
+                const blob = new Blob([res], {type: file.type});
+                // const url = window.URL.createObjectURL(blob);
+                // window.open(url);
+                const url: string = URL.createObjectURL(blob);
+                window.open(url,file.filename);
 
+            },
+            error => console.log('Error downloading the file.')
+            );
     }
 
     downloadAllegato(file): void{
