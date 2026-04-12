8de6dea851073136b8464d9367801c2bac88a85e
giuseppefiscarelli <57521642+giuseppefiscarelli@users.noreply.github.com>
2026-04-12 16:58:22 +0200

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  endpoint: 'https://inv10-api.giuseppefiscarelli.it',
+  endpoint: 'https://inv10-api.ramspa.it',
   baseApi: null,
   mfa:false
 };
